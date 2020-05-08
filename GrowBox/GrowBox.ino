8b829a31433f727a80e0c4ee3fa99297f0c6b2e1
bwsh <badworkshoper@gmail.com>
2020-05-08 09:46:56 +0300

    Init commit

---
+++ GrowBox/GrowBox.ino
@@ -0,0 +1,17 @@
+/*
+ Name:		GrowBox.ino
+ Created:	07.05.2020 22:29:01
+ Author:	badworkshoper
+*/
+
+// the setup function runs once when you press reset or power the board
+#include <GyverRelay.h>
+
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
