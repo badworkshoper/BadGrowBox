4c149e9a8d00d8cea571b92ed4d8698b13066638
bwsh <badworkshoper@gmail.com>
2021-02-23 10:36:05 +0200

    light

--- BadLight/BadLight.ino
+++ BadLight/BadLight.ino
@@ -1,12 +1,32 @@
 #define LEDPIN 3
 #define IRPIN 4
 
+int work_time = 1000*60*2;
+unsigned long time = 0;
+bool isLight = false;
+
+
+
 void setup(){
   pinMode(LEDPIN, OUTPUT);
   pinMode(IRPIN, INPUT);
 }
 
 void loop(){
+  if(!isLight){
+    if(digitalRead(IRPIN)){
+    time = millis() + work_time
+    }
+  }
+  
+  if(millis() <= time){
+    digitalWrite(LEDPIN, HIGH);
+  }
+  
+  
+  if(millis() >= time_now + t){
+      time_now += t;
+    }
   int value = digitalRead(IRPIN);
    
   switch(bool(value)){
