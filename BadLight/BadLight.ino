723e219300800e2c82223e2a434a3a2254a9988d
bwsh <badworkshoper@gmail.com>
2021-03-22 17:48:51 +0200

    fix light

--- BadLight/BadLight.ino
+++ BadLight/BadLight.ino
@@ -1,8 +1,10 @@
 #define LEDPIN 3
 #define IRPIN 4
+#define work_min 3
 
-int work_time = 1000*60*2;
+unsigned long work_time = 1000L * 60 * work_min;
 unsigned long time = 0;
+bool isChanged = false;
 bool isLight = false;
 
 
@@ -10,34 +12,26 @@ bool isLight = false;
 void setup(){
   pinMode(LEDPIN, OUTPUT);
   pinMode(IRPIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(IRPIN), onChange, CHANGE);
+}
+
+void onChange(){
+  isChanged = true;
 }
 
 void loop(){
-  if(!isLight){
-    if(digitalRead(IRPIN)){
-    time = millis() + work_time
-    }
-  }
-  
-  if(millis() <= time){
-    digitalWrite(LEDPIN, HIGH);
-  }
-  
-  
-  if(millis() >= time_now + t){
-      time_now += t;
-    }
-  int value = digitalRead(IRPIN);
-   
-  switch(bool(value)){
-    case true:
-      digitalWrite(LEDPIN, HIGH);
-      delay(20000);
-      break;
-    case false:
+  if(millis() > time){
+    if(isLight){
       digitalWrite(LEDPIN, LOW);
-      delay(50);
-      break;
+      isLight = false;
+    }
+  }
+  if(isChanged){
+    time = millis() + work_time;
+    if(!isLight){
+      digitalWrite(LEDPIN, HIGH);
+      }
+    isChanged = false;
   }
   
 }
